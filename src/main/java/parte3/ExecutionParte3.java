20b7d546b1bf362ba29d97051171018e896cc372
GasparPSousa <gasparufrj@gmail.com>
2021-03-20 10:58:52 -0300

    Utilizando o metodo find do entityManager

--- src/main/java/parte3/ExecutionParte3.java
+++ src/main/java/parte3/ExecutionParte3.java
@@ -1,4 +1,52 @@
 package parte3;
 
+import classes.Aluno;
+import classes.Estado;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ExecutionParte3 {
+
+    public static void main(String[] args) {
+
+        // 1 - Dados instanciados para serem instanciados como exemplo
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("parte2-DIO");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Estado estadoParaAdicionar = new Estado("Rio de Janeiro", "RJ");
+        entityManager.persist(estadoParaAdicionar);
+        entityManager.persist(new Estado("São Paulo", "SP"));
+        entityManager.persist(new Aluno("Daniel", 29, estadoParaAdicionar));
+        entityManager.persist(new Aluno("João", 20, estadoParaAdicionar));
+        entityManager.persist(new Aluno("Pedro", 30, estadoParaAdicionar));
+        entityManager.getTransaction().commit();
+
+        // 2 - Vamos utilizar o método do EntityManager find(), SQL nativo, JPQL e JPA Criteria API para realizar uma
+        // consulta que retornarar o mesmo valor equivalente aos seguintes SQL:
+
+        // SELECT * FROM Aluno WHERE id = 1 (Equivalente ao método find do entityManager na parte 2.2)
+        // SELECT * FROM Aluno WHERE nome = 'Daniel' (Sera o equivalente para as outras consultas nas partes 2.3 - 2.4 - 2.5)
+
+        // 2.1 O parametro de busca que será utilizado nas proximas consultas
+        String nome = "Daniel";
+
+        // =============================================================================================================
+
+        // 2.2 - Utilizando o método find do entityManager
+        // Trazendo somente 1 resultado
+        Aluno alunoEntityManager = entityManager.find(Aluno.class, 1);
+
+        // Trazendo uma lista como resultado
+        // Nao eh possivel!!! Deve utilizar um dos métodos utilizados abaixos nas partes 2.3 - 2.4 - 2.5
+
+        // Resultados das consultas acima
+        System.out.println("Consulta alunoEntityManager: " + alunoEntityManager);
+
+
+
+
+
+    }
 }
